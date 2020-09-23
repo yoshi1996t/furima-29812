df32a35c202cc7cd4abc9a0d837eae3620278bc0
yoshi1996t <am.k.fs.sea0702@gmail.com>
2020-09-23 11:42:28 +0900

    各モデルの作成（セレクト用）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
   belongs_to:user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shipping_fee_status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :schduled_delivery
+
+  #空の投稿を保存できないようにする
+  validates :category, :sales_status, :shipping_fee_status, :prefecture, :schduled_delivery, presence: true
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :genre_id, numericality: { other_than: 1 } 
 end
---
+++ spec/factories/schduled_deliveries.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :schduled_delivery do
+    
+  end
+end
