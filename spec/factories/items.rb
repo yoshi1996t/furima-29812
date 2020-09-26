05ea863ec03775004c10e7a27bf9eed5b31c9393
yoshi1996t <am.k.fs.sea0702@gmail.com>
2020-09-26 16:50:57 +0900

    itemモデルの単体テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {'テスト商品'}
+    info {'テスト商品の説明'}
+    category_id {2}
+    status_id {2}
+    postage_id {2}
+    prefecture_id {2}
+    schduled_delivery_id {2}
+    price {1000}
+    association :user
+
+    after(:build) do |content|
+      content.image.attach(io: File.open('public/images/test_image.jpeg'), filename: 'test_image.png')
+    end
   end
 end
