e60c0d9642c0dda9d51eb569f124328111367442
yoshi1996t <am.k.fs.sea0702@gmail.com>
2020-09-27 13:36:46 +0900

    コメントアウト削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  # get 'items/index'
   root to: 'items#index'
   devise_for :users
   resources :items, only: [:new, :create]
-  # devise_for :users, :controllers => {
-  #   :registrations => 'users/registrations',
-  #   :sessions => 'users/sessions'   
-  # } 
-  # resources :user
 end
