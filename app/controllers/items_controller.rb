d744b43186f4b633dfdf8876dd143c29db636476
yoshi1996t <am.k.fs.sea0702@gmail.com>
2020-09-27 11:23:31 +0900

    ページ遷移の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: :new
+
   def index
   end
 
@@ -8,11 +10,21 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
 
   private
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :status_id, :postage_id,:prefecture_id, :schduled_delivery_id, :price).merge(user_id: current_user.id)
   end 
+
+  def move_to_index
+    redirect_to new_user_session_path
+  end
 
 end
