e45a4d9a3991294a349eafcbf8a0f116b092ac12
yoshi1996t <am.k.fs.sea0702@gmail.com>
2020-10-05 16:16:20 +0900

    バリデーションの制限を一括で設定

--- app/models/order_shape.rb
+++ app/models/order_shape.rb
@@ -2,14 +2,16 @@ class OrderShape
   include ActiveModel::Model
 
   attr_accessor :post_code, :prefecture_id, :city, :municipality, :building_name, :phone_number, :user_id, :item_id, :token
+  
+  with_options presence: true do
+      validates :city, :municipality, :token
+      
+      validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
 
-  validates :post_code, :prefecture_id, :city, :municipality, :phone_number, :token, presence: true
+      validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
-  validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-
-  validates :phone_number, format: { with: /\A[0-9]+\z/ }
+      validates :phone_number, format: { with: /\A[0-9]+\z/ }
+  end
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
